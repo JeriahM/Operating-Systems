26f649ac37e108f9fb033f9f9accf23b2bcddedf
Jeriah Muleya <jmuleya@nd.edu>
2023-03-05 17:37:22 -0500

    Update Milestone02 for input split

--- Milestone02/singleshell.c
+++ Milestone02/singleshell.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include <unistd.h>
 #include <signal.h>
 
@@ -11,8 +13,21 @@ int main(int argc, char *argv[]){
 		
 	fprintf(stdout, "Execute: ");
 	/* Get command for code to run */
-	char command[BUFSIZ];
-	fgets(command, BUFSIZ, stdin);
+	char input[BUFSIZ];
+	int i, spaces=1;
+	fgets(input, BUFSIZ, stdin);
+	/* Count number of spaces */
+	for(i=0;i<strlen(input);i++){
+		spaces += isspace(input[i]);
+	}
+	char *args[spaces];
+	/* split input into arguments */
+	for(i=0;i<spaces;i++){
+		if(i==0)
+			args[i] = strtok(input, " ");
+		else
+			args[i] = strtok(NULL, " \n");
+	}
 
 	int rc = fork();
 	if (rc < 0){
@@ -21,10 +36,10 @@ int main(int argc, char *argv[]){
 	}
 	else if (rc == 0){
 		/* Execute command */
-		fprintf(stdout, "Executing: %s\n", command);
+		fprintf(stdout, "Executing: %s\n", args[0]);
 		
 		/* Output results */
-		execvp(command);
+		execvp(args[0], args);
 
 		fprintf(stdout, "Execution complete!\n");
 	}
